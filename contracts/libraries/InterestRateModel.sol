e911eb8ee70a21bd0c1fc171b2652e0cde0db594
moelhaj996 <mohsuliman.996@gmail.com>
2025-09-27 00:04:23 +0200

    update

---
+++ contracts/libraries/InterestRateModel.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "../interfaces/IInterestRateModel.sol";
+
+contract InterestRateModel is IInterestRateModel {
+    uint256 public constant BASE_RATE_PER_YEAR = 0.02e18;
+    uint256 public constant MULTIPLIER_PER_YEAR = 0.1e18;
+    uint256 public constant JUMP_MULTIPLIER_PER_YEAR = 0.5e18;
+    uint256 public constant KINK = 0.8e18;
+    uint256 private constant SECONDS_PER_YEAR = 365 days;
+
+    function utilizationRate(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves
+    ) public pure override returns (uint256) {
+        if (borrows == 0) {
+            return 0;
+        }
+        return (borrows * 1e18) / (cash + borrows - reserves);
+    }
+
+    function getBorrowRate(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves
+    ) public view override returns (uint256) {
+        uint256 util = utilizationRate(cash, borrows, reserves);
+
+        if (util <= KINK) {
+            return (util * MULTIPLIER_PER_YEAR) / 1e18 + BASE_RATE_PER_YEAR;
+        } else {
+            uint256 normalRate = (KINK * MULTIPLIER_PER_YEAR) / 1e18 + BASE_RATE_PER_YEAR;
+            uint256 excessUtil = util - KINK;
+            return (excessUtil * JUMP_MULTIPLIER_PER_YEAR) / 1e18 + normalRate;
+        }
+    }
+
+    function getSupplyRate(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves,
+        uint256 reserveFactor
+    ) public view override returns (uint256) {
+        uint256 oneMinusReserveFactor = 1e18 - reserveFactor;
+        uint256 borrowRate = getBorrowRate(cash, borrows, reserves);
+        uint256 rateToPool = (borrowRate * oneMinusReserveFactor) / 1e18;
+        return (utilizationRate(cash, borrows, reserves) * rateToPool) / 1e18;
+    }
+
+    function getBorrowRatePerBlock(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves
+    ) external view returns (uint256) {
+        return getBorrowRate(cash, borrows, reserves) / (SECONDS_PER_YEAR / 12);
+    }
+
+    function getSupplyRatePerBlock(
+        uint256 cash,
+        uint256 borrows,
+        uint256 reserves,
+        uint256 reserveFactor
+    ) external view returns (uint256) {
+        return getSupplyRate(cash, borrows, reserves, reserveFactor) / (SECONDS_PER_YEAR / 12);
+    }
+}
---
+++ contracts/libraries/LiquidationMath.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+library LiquidationMath {
+    uint256 public constant PRECISION = 1e18;
+    uint256 public constant LIQUIDATION_CLOSE_FACTOR = 0.5e18;
+
+    function calculateHealthFactor(
+        uint256 collateralValue,
+        uint256 debtValue,
+        uint256 liquidationThreshold
+    ) internal pure returns (uint256) {
+        if (debtValue == 0) return type(uint256).max;
+        return (collateralValue * liquidationThreshold) / (debtValue * PRECISION);
+    }
+
+    function isLiquidatable(uint256 healthFactor) internal pure returns (bool) {
+        return healthFactor < PRECISION;
+    }
+
+    function calculateLiquidationAmounts(
+        uint256 debtToCover,
+        uint256 totalDebt,
+        uint256 totalCollateral,
+        uint256 liquidationBonus
+    ) internal pure returns (uint256 collateralToLiquidate, uint256 actualDebtCovered) {
+        uint256 maxDebtToCover = (totalDebt * LIQUIDATION_CLOSE_FACTOR) / PRECISION;
+        actualDebtCovered = debtToCover > maxDebtToCover ? maxDebtToCover : debtToCover;
+
+        collateralToLiquidate = (actualDebtCovered * totalCollateral * (PRECISION + liquidationBonus)) /
+            (totalDebt * PRECISION);
+    }
+
+    function calculateCollateralValue(
+        uint256 collateralAmount,
+        uint256 collateralPrice,
+        uint256 collateralDecimals
+    ) internal pure returns (uint256) {
+        return (collateralAmount * collateralPrice) / (10 ** collateralDecimals);
+    }
+
+    function calculateDebtValue(
+        uint256 debtAmount,
+        uint256 debtPrice,
+        uint256 debtDecimals
+    ) internal pure returns (uint256) {
+        return (debtAmount * debtPrice) / (10 ** debtDecimals);
+    }
+
+    function calculateMaxBorrow(
+        uint256 collateralValue,
+        uint256 collateralFactor,
+        uint256 currentDebt
+    ) internal pure returns (uint256) {
+        uint256 maxBorrowValue = (collateralValue * collateralFactor) / PRECISION;
+        return maxBorrowValue > currentDebt ? maxBorrowValue - currentDebt : 0;
+    }
+}
